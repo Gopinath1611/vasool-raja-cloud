88cdba294c5716338207e3abc7d9c74a4de0c205
Gopinath1611 <117503683+Gopinath1611@users.noreply.github.com>
2026-08-22 16:41:44 +0530

    Add tnDistricts constant for Tamil Nadu locations

---
+++ src/constants/tnLocations.js
@@ -0,0 +1,33 @@
+// src/constants/tnLocations.js
+
+export const tnDistricts = {
+  "Salem": [
+    "Salem Taluk", "Attur", "Gangavalli", "Edappadi", 
+    "Mettur", "Omalur", "Pethanaickenpalayam", "Sankagiri", "Yercaud"
+  ],
+  "Tiruchirappalli (Trichy)": [
+    "Srirangam", "Trichy West", "Trichy East", "Thuraiyur", 
+    "Lalgudi", "Manachanallur", "Musiri", "Manapparai", "Tiruverumbur"
+  ],
+  "Namakkal": [
+    "Namakkal", "Rasipuram", "Tuchengode", "Paramathi Velur", "Komarapalayam", "Kolli Hills"
+  ],
+  "Erode": [
+    "Erode", "Bhavani", "Gobichettipalayam", "Sathyamangalam", "Perundurai", "Modakurichi"
+  ],
+  "Coimbatore": [
+    "Coimbatore North", "Coimbatore South", "Annur", "Mettupalayam", 
+    "Pollachi", "Sulur", "Valparai"
+  ],
+  "Madurai": [
+    "Madurai North", "Madurai South", "Madurai West", "Madurai East",
+    "Melur", "Peraiyur", "Thirumangalam", "Tirupparankundram", "Usilampatti"
+  ],
+  "Thanjavur": [
+    "Thanjavur", "Kumbakonam", "Papanasam", "Pattukkottai", "Peravurani", "Thiruvaiyaru", "Orathanadu"
+  ],
+  "Chennai": [
+    "Egmore", "Guindy", "Mylapore", "Perambur", 
+    "Purasawalkam", "Sholinganallur", "T. Nagar", "Thiruvottiyur", "Tondiarpet", "Velachery"
+  ]
+};
