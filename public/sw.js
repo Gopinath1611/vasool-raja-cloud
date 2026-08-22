649a82f7f74c5f8a1cf82a9de55e8aa7dbc47b8e
Gopinath1611 <117503683+Gopinath1611@users.noreply.github.com>
2026-08-22 16:22:59 +0530

    Add service worker for caching assets

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'vasool-raja-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Fetch/Cache Request
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
